97a31626d1bd9b00c12ca35c0014a182539b777e
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2022-09-22 10:19:02 -0300

    feat: implementa memo para o component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Image, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -8,28 +9,47 @@ import infoIcon from '../../assets/images/icons/info.png';
 import styles from './styles';
 
 interface ProductProps {
+
     title: string,
-    imageURL: string,
+    imageURL?: string | undefined,
     link: (pointerInside: boolean) => void,
     column?: Boolean;
+
 }
 
-const Product: React.FC<ProductProps> = (props) => {
-
+function ProductComponent ({title, imageURL, link, column}:  ProductProps): JSX.Element {
 
     return (
-        <View style={[styles.product, props.column && styles.productLineColumn]}>
-            <BorderlessButton style={styles.productLink} onPress={props.link}/>
-            <View style={[styles.productBg, props.column && styles.productBgColumn]}>
-                <Image style={[styles.productImage, props.column && styles.productImageColumn]} source={{uri: props.imageURL}} />
-                <Text style={styles.productText}>
-                    {props.title}
+        <View style={[styles.product, column ? styles.productLineColumn : styles.productGridItem]}>
+            <BorderlessButton style={styles.productLink} onPress={link}/>
+            <View style={[styles.productBg, column && styles.productBgColumn]}>
+                {imageURL && (
+                    <Image style={[styles.productImage, column && styles.productImageColumn]} source={{uri: imageURL}} />
+                )}
+                {! imageURL && (
+                    <FontAwesome 
+                        name='image' 
+                        size={37}
+                        style={[
+                            styles.productImage, 
+                            column && styles.productImageColumn,
+                            {color: '#004254'}
+                        ]}
+                    />
+                 )}
+                <Text 
+                    style={[styles.productText, column && styles.productBgColumnText]} 
+                    numberOfLines={2}
+                >
+                    {title}
                 </Text>
-                <Image style={[styles.productIcon, props.column && styles.productIconColumn]} source={infoIcon}/>
+                <Image style={[styles.productIcon, column && styles.productIconColumn]} source={infoIcon}/>
             </View>
         </View>
     )
 
 }
 
-export default Product;
+export const Product = memo(ProductComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps, nextProps);
+})
