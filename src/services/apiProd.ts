e520a79a7078052ed7e2ea4ec4f2b2e04897d76d
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2023-01-26 18:04:44 -0300

    fix

--- src/services/apiProd.ts
+++ src/services/apiProd.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosError } from 'axios';
-import { WP_CUSTOMER_KEY, WP_CUSTOMER_SECRET, WP_BASE_URL } from '@env'
+// import { WP_CUSTOMER_KEY, WP_CUSTOMER_SECRET, WP_BASE_URL } from '@env'
 
-const apiProd = axios.create({ baseURL: WP_BASE_URL });
+const apiProd = axios.create({ baseURL: process.env.WP_BASE_URL });
 
 apiProd.interceptors.request.use(
     config => {
         config.params = {
             ...config.params,
-            consumer_key: WP_CUSTOMER_KEY,
-            consumer_secret: WP_CUSTOMER_SECRET
+            consumer_key: process.env.WP_CUSTOMER_KEY,
+            consumer_secret: process.env.WP_CUSTOMER_SECRET
         }
         return config;
     },
