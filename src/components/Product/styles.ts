12daee184900955ebbcc3a8ef63a8b1bb67a4018
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2023-06-02 16:45:01 -0300

    fix

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40,
         zIndex: 999,
+        resizeMode: 'contain',
         position: 'absolute',
         top: -10,
     },
